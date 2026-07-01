5af6367462e8a2869af336c816d6f185a26968ee
Elena Savitskaya <helena0518savitskaya@gmail.com>
2026-07-01 18:05:36 +0300

    toast direction

--- lib/toast.tsx
+++ lib/toast.tsx
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 export function toast(type: TypeOptions, children: React.ReactNode) {
   origin(children, {
     type,
-    position: "bottom-right",
+    position: "bottom-left",
   });
 }
 
